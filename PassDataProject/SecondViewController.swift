498c8bba4fa877cb2759e74a4402962bd55dcc8a
deshabml <deshab@yandex.ru>
2022-11-26 14:26:42 +0300

    Segue go to SecondViewController - completed

--- PassDataProject/SecondViewController.swift
+++ PassDataProject/SecondViewController.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var login: String?
     @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let login = self.login else { return }
+        label.text = "Hellow, \(login)"
+    }
     
     @IBAction func goBackTapped(_ sender: UIButton) {
+        
     }
 }
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTF: UITextField!
 
     @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSague", sender: nil)
+
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginTF.text
+        
     }
 }
 
