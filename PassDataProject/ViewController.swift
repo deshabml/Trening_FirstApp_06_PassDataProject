f01e8541ae2708639b46ff40e8366a126d4bf4d9
deshabml <deshab@yandex.ru>
2022-11-26 12:58:16 +0300

    Initial Commit

---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Лаборатория on 26.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
