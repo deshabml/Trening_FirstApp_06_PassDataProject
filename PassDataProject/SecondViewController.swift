361107e6bb6b255e11f170543ff9e48d13a81ede
deshabml <deshab@yandex.ru>
2022-11-26 14:01:28 +0300

    Second ViewController Constaints completed and connections are set up

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Лаборатория on 26.11.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+}
